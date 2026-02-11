178c247904cced2dccec2ae0b8d75a275562340d
JNaniBabu <jakkulananibabu143@gmail.com>
2026-02-11 23:51:40 +0530

    changes has been made

--- Authentication/management/commands/create_initial_superuser.py
+++ Authentication/management/commands/create_initial_superuser.py
@@ -1,18 +1,20 @@
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from Authentication.models import User
+from django.contrib.auth import get_user_model
+import os
 
+User = get_user_model()
 
 class Command(BaseCommand):
-    help = 'Creates an initial superuser from env variables'
+    help = 'Create initial superuser from env variables'
 
     def handle(self, *args, **options):
-        email = settings.DJANGO_SUPERUSER_EMAIL
-        password = settings.DJANGO_SUPERUSER_PASSWORD
-        name = settings.DJANGO_SUPERUSER_USERNAME
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        name = os.environ.get('DJANGO_SUPERUSER_NAME')
 
         if not User.objects.filter(email=email).exists():
             User.objects.create_superuser(email=email, password=password, name=name)
             self.stdout.write(self.style.SUCCESS(f'Superuser {email} created'))
         else:
-            self.stdout.write('Superuser already exists')
+            self.stdout.write(self.style.WARNING(f'Superuser {email} already exists'))
