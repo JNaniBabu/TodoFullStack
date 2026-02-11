8d6f287311a785332b81749e03e0d3a4eef33566
JNaniBabu <jakkulananibabu143@gmail.com>
2026-02-12 00:42:52 +0530

    changes has been made

--- Backend/settings.py
+++ Backend/settings.py
@@ -87,13 +87,15 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-production-frontend.up.railway.app"
+    "https://web-production-b7c02.up.railway.app/"
+    
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-production-frontend.up.railway.app"
+    "https://web-production-b7c02.up.railway.app/"
+    
     
 ]
 
