ee25cd096cf097e11d6d7be6a04d931de9cc4d28
JNaniBabu <jakkulananibabu143@gmail.com>
2026-02-13 19:13:15 +0530

    changes

--- Backend/settings.py
+++ Backend/settings.py
@@ -81,29 +81,15 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-# ALLOWED_HOSTS = ["web-production-b7c02.up.railway.app"]
+ALLOWED_HOSTS = ["web-production-b7c02.up.railway.app"]
+
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://todo-full-stack-lbax.vercel.app"
-    
-]
-
-ALLOWED_HOSTS = ["*"]  
-
-# CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://todo-full-stack-lbax.vercel.app",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
