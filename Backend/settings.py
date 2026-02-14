c03866731acd1be7e26ee7cbaec70c9161c619ff
JNaniBabu <jakkulananibabu143@gmail.com>
2026-02-14 11:24:27 +0530

    c

--- Backend/settings.py
+++ Backend/settings.py
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ROTATE_REFRESH_TOKENS": True,
